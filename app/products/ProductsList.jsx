ae9f9b59072692dad415efe016c98cdc5df58181
AgustinDi <agustindigiacinto18@gmail.com>
2023-05-27 18:35:00 -0300

    x

--- app/products/ProductsList.jsx
+++ app/products/ProductsList.jsx
@@ -8,7 +8,7 @@ export default async function ProductsList() {
             |---------------|
             {data.map(value => 
                 <div key={value.name}>
-                    <Image src={value.image} width={100} height={100} alt={value.title}/>
+                    <Image src={value.image} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" width={100} height={100} alt={value.title}/>
                     <h3>
                         {value.name}
                     </h3>
