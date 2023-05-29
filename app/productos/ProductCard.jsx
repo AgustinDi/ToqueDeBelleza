5ad863d694579ba9f72d919f7b603743347037b5
AgustinDi <agustindigiacinto18@gmail.com>
2023-05-29 12:39:03 -0300

    styled products cards

--- app/productos/ProductCard.jsx
+++ app/productos/ProductCard.jsx
@@ -4,13 +4,13 @@ import css from './ProductCard.module.css';
 export default function ProductCard({value}){
     return (
         <div key={value.name} className={css.card}>
-            <Image src={value.image} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" width={100} height={100} alt={value.name}/>
+            <Image src={value.image} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" width={150} height={150} alt={value.name}/>
             <div className={css.title}>
                 <h3>
-                    {value.name} {value.price + '$'}
+                    {value.name} 
                 </h3>
                 <h4>
-                    {value.size}
+                    {value.size} - {value.price + '$'}
                 </h4>
             </div>
             {/* <div>          ESTA DATA ES PARA EL DETALLE
