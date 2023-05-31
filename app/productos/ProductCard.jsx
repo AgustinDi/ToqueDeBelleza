a7500e183c3abddd9863826bf2336f828c5a8de5
AgustinDi <agustindigiacinto18@gmail.com>
2023-05-31 17:27:37 -0300

    Products.json updated

--- app/productos/ProductCard.jsx
+++ app/productos/ProductCard.jsx
@@ -1,12 +1,26 @@
 import Image from "next/image";
 import css from './ProductCard.module.css';
 import Link from "next/link";
+import loadingImage from '../../public/Loading.png';
+
+
+// {
+//     "name": "",
+//     "category": "",
+//     "price": 800,
+//     "size": "250ml",
+//     "description": "",
+//     "howToUse": "",
+//     "precaution": "",
+//     "aplication": "",
+//     "image": ""
+// }
 
 export default function ProductCard({value}){
     return (
         <div key={value.name} className={css.card}>
             <Link href={'/producto/' + value.name}>
-            <Image src={value.image} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" width={150} height={150} alt={value.name}/>
+            <Image src={value.image || loadingImage} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" width={150} height={150} alt={value.name}/>
             <div className={css.title}>
                 <h3>
                     {value.name} 
