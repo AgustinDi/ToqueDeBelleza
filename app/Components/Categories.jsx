b81d76c945059f7f36d782b84d1a8fed2ab1a4d9
AgustinDi <agustindigiacinto18@gmail.com>
2023-06-05 16:29:35 -0300

    added Link to category images

--- app/Components/Categories.jsx
+++ app/Components/Categories.jsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
 import css from './Categories.module.css';
+import Link from 'next/link';
 
 function Category({value}){
     return (
         <article className={css.category}>
+            <Link href={'/productos/' + value.code}>
             <Image src='/Loading.png' alt={value.name} width={160} height={200}/>
             <p style={{top: '-100px'}} className={css.categoryTitle}>
                 {value.name}
             </p>
+            </Link>
         </article>
     )
 }
