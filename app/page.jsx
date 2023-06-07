0c484429fe20d65f35ff666e7585b851891cc510
AgustinDi <agustindigiacinto18@gmail.com>
2023-06-07 10:27:56 -0300

    ProducList on home

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Categories from "./Components/Categories.jsx";
+import ProductsList from "./productos/ProductsList.jsx";
 
 export default function initialApp(){
     
@@ -7,6 +8,7 @@ export default function initialApp(){
             <h1>Toque de belleza - Next.js 13 App</h1>
             <p>Proyecto Personal</p>
             <Categories />
+            <ProductsList amount={4}/> 
             <div style={{height: '50vh'}}></div>
         </>
     )
--- app/productos/ProductsList.jsx
+++ app/productos/ProductsList.jsx
@@ -2,8 +2,9 @@ import productsJson from './Products.json'
 import ProductCard from './ProductCard.jsx'
 import css from './ProductList.module.css'
 
-export default async function ProductsList({ filter, orderby }) {
-    const dataFiltered = productsJson.data?.filter(product => filter ? product.category === filter : true)
+export default async function ProductsList({ filter, amount }) {
+    let dataFiltered = productsJson.data?.filter(product => filter ? product.category === filter : true)
+    if(amount) dataFiltered = dataFiltered.slice(0, amount) 
 
     return (
         <div>
