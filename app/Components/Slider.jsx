56bf4e54a20407a76a374cfd552e17daf40e4b49
AgustinDi <agustindigiacinto18@gmail.com>
2023-06-09 14:59:01 -0300

    Slider working

---
+++ app/Components/Slider.jsx
@@ -0,0 +1,93 @@
+'use client'
+
+import Image from 'next/image';
+import css from './Slider.module.css';
+import { useEffect, useState, useRef } from 'react';
+
+export default function Slider() {
+    const cardWidth = 700;
+    const sliderRef = useRef(null);
+    const [transformSlider, setTransformSlider] = useState(0);
+    const [transitionSlider, setTransitionSlider] = useState('transform 1s');
+    const [slideCounter, setSlideCounter] = useState(0);
+    const [isInTransition, setIsTransition] = useState(false);
+    const [images, setImages] = useState([
+        {
+            src: "/Loading.png",
+            name: "agua 1",
+        },
+        {
+            src: "/Loading.png",
+            name: "fuego 2",
+        },
+        {
+            src: "/Loading.png",
+            name: "planta 3",
+        },
+        {
+            src: "/Loading.png",
+            name: "aire 4",
+        },
+    ])
+    
+
+    useEffect(()=>{
+        reorderSlider();
+    },[])
+
+    async function reorderSlider(){
+        // funcion para reordenar las imagenes para el slide infinito
+        const updatedImages = []
+        setTransitionSlider('none')
+        if(slideCounter == images.length -1){ //estamos en el ultimo elemento
+        const firstImage = images[0];
+        images.forEach((item, i) => {
+            if(i !== 0) updatedImages.push(item)
+            if(i === images.length -1) updatedImages.push(firstImage)
+        })
+        setImages(updatedImages);
+        setTransformSlider(value => value + cardWidth)
+        setSlideCounter(value=> value - 1)
+        } else if(slideCounter == 0){  // estamos en el primer elemento
+        const lastImage = images[images.length - 1];
+        images.forEach((item, i) => {
+            if(i !== images.length -1){
+                updatedImages.push(item)
+            } else {
+                updatedImages.unshift(lastImage)
+            }
+        })
+        setImages(updatedImages);
+        setTransformSlider(value => value - cardWidth)
+        setSlideCounter(value=> value + 1)
+        }
+        setIsTransition(false)
+    }
+
+    function moveSlide(direction){
+        if(isInTransition) return
+        // true es derecha, left es izquierda
+        setTransitionSlider('transform 1s')
+        setIsTransition(true)
+        if(direction){ 
+            setTransformSlider(value => value - cardWidth)
+            setSlideCounter(value=> value + 1)
+        } else {
+            setTransformSlider(value => value + cardWidth)
+            setSlideCounter(value=> value - 1)
+        }
+    }
+
+  return (
+    <div className={css.sliderContainer}>
+        <div ref={sliderRef} className={css.slider} style={{transform: `translateX(${transformSlider}px)`,transition: transitionSlider}} onTransitionEnd={reorderSlider}>
+             {images && images.map(({src, name},i)=> <div key={i} className={css.sliderElement}><Image fill src={src} alt={i}/><p style={{position: 'absolute', zIndex: '1000', color: 'green'}}>{name}</p></div>)}
+        </div>
+        <div className={css.sliderButtons}>
+            <button className={`${css.button} ${css.buttonLeft}`} onClick={()=>moveSlide(false)}>{'<'}</button>
+            {slideCounter}
+            <button className={`${css.button} ${css.buttonRight}`}  onClick={()=>moveSlide(true)}>{'>'}</button>
+        </div>
+    </div>
+  )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Categories from "./Components/Categories.jsx";
+import Slider from "./Components/Slider.jsx";
 import ProductsList from "./productos/ProductsList.jsx";
 
 export default function initialApp(){
@@ -7,6 +8,7 @@ export default function initialApp(){
         <>
             <h1>Toque de belleza - Next.js 13 App</h1>
             <p>Proyecto Personal</p>
+            <Slider /> 
             <Categories />
             <ProductsList amount={5}/> 
             <div style={{height: '50vh'}}></div>
