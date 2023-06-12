39bdfce7696ebd78796103e1a803182cc32df65c
AgustinDi <agustindigiacinto18@gmail.com>
2023-06-12 18:51:18 -0300

    "agregar al carrito" fix

--- app/productos/AddToCart.jsx
+++ app/productos/AddToCart.jsx
@@ -6,7 +6,7 @@ export default function AddToCart({ value }) {
     const { addToCart } = useMyContext();
     
     return (
-        <button onClick={() => {addToCart(value);alert(`Se a agregado ${value.name} al carrito!`)}}>
+        <button onClick={() => {addToCart(value.name);alert(`Se a agregado ${value.name} al carrito!`)}}>
             Agregar al carrito
         </button>
     )
