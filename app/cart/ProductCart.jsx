d6857e4884466c744bfc10ad9a5f3368e1cc1b87
AgustinDi <agustindigiacinto18@gmail.com>
2023-06-15 18:28:11 -0300

    productCart Links added

--- app/cart/ProductCart.jsx
+++ app/cart/ProductCart.jsx
@@ -3,6 +3,8 @@
 import css from "./ProductCart.module.css"
 import Image from "next/image";
 import { UseMyContext } from "../Context/Context"
+import Link from "next/link";
+import Categories from "../Components/Categories";
 
 export default function ProductCart({ data }) {
   const { addToCart, removeToCart, minusToCart} = UseMyContext();
@@ -10,24 +12,30 @@ export default function ProductCart({ data }) {
     return (
     <div className={css.productCart}>
       <div>
-        <div className={css.imageContainer}>
-          <Image src={data.image} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt={data.name}/>
-        </div>
+        <Link  href={`/producto/${data.name}`} >
+          <div className={css.imageContainer}>
+            <Image src={data.image} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt={data.name}/>
+          </div>
+        </Link>
       </div>
         <div className={css.sideInfo}>
           <div className={css.topInfo}>
             <div className={css.titleInfo}>
+              <Link  href={`/producto/${data.name}`}>
               <h3>
                 {data.name}
               </h3>
+              </Link>
               <h4>
                 {data.size} 
               </h4>
             </div>
             <div>
+              <Link  href={`/productos/${data.category}`}>
               <span>
                 {data.category}  
               </span> 
+              </Link>
               <button onClick={()=> removeToCart(data.name)}>X</button>
             </div>
           </div>
