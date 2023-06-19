262d559e0d29c0a5e3ca6590c72286023a42ee59
AgustinDi <agustindigiacinto18@gmail.com>
2023-06-19 20:44:31 -0300

    productDetail css updated

--- app/producto/[name]/page.jsx
+++ app/producto/[name]/page.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
 import Image from 'next/image';
+import css from './page.module.css';
 import productsJson from '../../productos/Products.json';
 import IndexV from '@/app/productos/IndexV';
+import Link from 'next/link';
 
 export default function producto({ params }) {
   const nameOfProduct = decodeURI(params.name)
   const product = productsJson.data?.find(value=> value.name === nameOfProduct)
 
   return (
-    <article>
+    <article className={css.article}>
       <IndexV category={product.category} individual={nameOfProduct}/>
-      <div>
-        <Image width={300} height={300} src={product.image} alt={product.name}/>
-        <div>
-          <h2>{product.name}</h2>
-          <span>{product.size}</span> - <span>{product.price}$</span>
-          <br/>
-          <button>Añadir al Carrito</button>
-          {/* este boton es un futuro componente 'use client' */}
+      <div className={css.containerPoduct}>
+        <div className={css.containerImage}>
+          <Image fill src={product.image} alt={product.name}/>
+        </div> 
+        <div className={css.sideInfo}>
+          <div className={css.titleProduct}>
+            <h2>{product.name}</h2>
+            <Link href={`/productos/${product.category}`}>
+              <h3>{product.category}</h3>
+            </Link>
+          </div> 
+          <h4>{product.size} - {product.price}$</h4>
+          <h5>Sobre {product.name}: <br/> {product.description}</h5>
+          <div className={css.buttonAddToCart}>
+          <button>Añadir al Carrito</button> {/* este boton es un futuro componente 'use client' */}
+          </div>
         </div>
       </div>
-      <div>
-        <p>sobre {product.name}: <br/> {product.description}</p>
-        {product.howToUse && (<p>modo de uso: <br/> {product.howToUse}</p>)}
-        {product.precaution && (<p>precauciones: <br/> {product.precaution}</p>)}
-        {product.aplication && (<p>aplicacion: <br/> {product.aplication}</p>)}
-      </div>
+      { product.howToUse || product.precaution 
+      ?
+      <div className={css.containerMoreDescription}>
+        {product.howToUse && (<div>modo de uso: <br/> {product.howToUse}</div>)}
+        {product.precaution && (<div>precauciones: <br/> {product.precaution}</div>)}
+      </div>      
+      : null}
     </article>
   )
 }
