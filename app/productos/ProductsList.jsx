267814c9fbbb9086f928cb4d04a8774b1190aa3a
AgustinDi <agustindigiacinto18@gmail.com>
2023-06-23 09:51:47 -0300

    css productList

--- app/productos/ProductsList.jsx
+++ app/productos/ProductsList.jsx
@@ -5,12 +5,14 @@ import css from './ProductList.module.css'
 export default async function ProductsList({ filter, amount }) {
     let dataFiltered = productsJson.data?.filter(product => filter ? product.category === filter : true)
     let styles = {}
-    // if(amount) {
-    //     dataFiltered = dataFiltered.slice(0, amount)
-    //     styles = {
-    //         gridTemplateColumns: `repeat(${amount}, 1fr)`
-    //     }
-    // }
+    if(amount) {
+        dataFiltered = dataFiltered.slice(0, amount)
+        styles = {
+            gridTemplateColumns: `repeat(${amount}, 1fr)`
+        }
+    }
+
+    // if(dataFiltered.length >= 4)
 
 
     return (
