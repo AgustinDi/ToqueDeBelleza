c95f5616601823efcffb3d08219e762eef764257
AgustinDi <agustindigiacinto18@gmail.com>
2023-06-23 21:57:28 -0300

    dinamic dot

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -72,7 +72,7 @@ export default function NavBar(){
                     )}
                     <li className={css.containerCartImage}>
                         <Link href={'/cart'} onClick={()=>setIsSearching(false)}>
-                            <div className={css.containerCartDot}>{hasProducts().length}</div>
+                            {hasProducts().length ? <div className={css.containerCartDot}>{hasProducts().length}</div> : null}
                             <Image className={css.cartImage} src={'/carrito.png'} fill alt="carro de compras"/>
                         </Link>
                     </li> 
