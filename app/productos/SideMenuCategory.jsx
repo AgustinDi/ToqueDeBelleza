d3a2774a28173ac1d7c19a8578dbf492fd886811
AgustinDi <agustindigiacinto18@gmail.com>
2023-07-04 21:19:19 -0300

    filter category absolute

--- app/productos/SideMenuCategory.jsx
+++ app/productos/SideMenuCategory.jsx
@@ -1,18 +1,29 @@
+'use client'
+
 import css from './SideMenuCategory.module.css';
 import Link from 'next/link';
 import productsJson from './Products.json';
+import { UseMyContext } from '../Context/Context';
 
 export default function SideMenuCategory() {
     const categoriesSet = new Set(productsJson.data.map(product => product.category))
     const categorias = [...categoriesSet];
+    const { isMobile } = UseMyContext();
 
     return (
         <div className={css.sideMenu}>
+            { !isMobile 
+            ? <>
             <h3>Filtro por Categoria:</h3>
             <ul className={css.ul}>
                 <Link href={'/productos/'}><li>Todos</li></Link>
                 {categorias.map(categoria => <Link key={categoria} href={'/productos/' + categoria}><li>{categoria}</li></Link>)}
             </ul>
+            </>
+            : <div className={css.mobileSideMenu}>
+
+            </div>
+            }
         </div>
   )
 }
