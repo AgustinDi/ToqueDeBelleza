8fcbb315f99926d417ab21ed51379935c73441fa
AgustinDi <agustindigiacinto18@gmail.com>
2023-07-10 12:26:48 -0300

    slider & sidemenu to css native

--- app/productos/SideMenuCategory.jsx
+++ app/productos/SideMenuCategory.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import css from './SideMenuCategory.module.css';
+import './SideMenuCategory.css';
 import Link from 'next/link';
 import productsJson from './Products.json';
 import { UseMyContext } from '../Context/Context';
@@ -26,23 +26,23 @@ export default function SideMenuCategory() {
     }
 
     return (
-        <div className={css.sideMenu}>
+        <div className='sidsideMenu'>
             { !isMobile 
             ? <>
             <h3>Filtro por Categoria:</h3>
-            <ul className={css.ul}>
+            <ul className='sidul'>
                 <Link href={'/productos/'}><li>Todos</li></Link>
                 {categorias.map(categoria => <Link key={categoria} href={'/productos/' + categoria}><li>{categoria}</li></Link>)}
             </ul>
             </>
-            : <div className={css.containerMobileSideMenu}>
-                <div className={css.mobileSideMenu}>
-                    <div className={css.titleMobileSideMenu}>
+            : <div className='sidcontainerMobileSideMenu'>
+                <div className='sidmobileSideMenu'>
+                    <div className='sidtitleMobileSideMenu'>
                         <h4 onClick={()=>setOpen(true)}>Categorias</h4> 
                         <button onClick={()=>setOpen(false)} style={open ? xActive : xInactive}>X</button>
                     </div>
                 </div>
-                <ul className={css.ul}style={open ? ulActive : ulInactive}>
+                <ul className='sidul' style={open ? ulActive : ulInactive}>
                     <Link  href={'/productos/'} onClick={()=>setOpen(false)}><li>Todos</li></Link>
                     {categorias.map(categoria => <Link key={categoria} href={'/productos/' + categoria} onClick={()=>setOpen(false)}><li>{categoria}</li></Link>)}
                 </ul>
