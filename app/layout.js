3c482e01f07ce46daa08a9725960c2cf293e282b
AgustinDi <agustindigiacinto18@gmail.com>
2023-07-12 12:19:26 -0300

    head - viewport added

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="es" className={montserrat.className}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </head>
       <body>
         <Context>
         <div style={{height: '100px'}}></div>
