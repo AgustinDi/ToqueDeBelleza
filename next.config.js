294ba8f6635fa096e957f0db4ee7d52a477335a9
AgustinDi <agustindigiacinto18@gmail.com>
2023-07-12 13:01:06 -0300

    fix & next.js css config changed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true
+    appDir: true,
+    optimizeCss: false
   },
   images: {
       domains: ['res.cloudinary.com']
   }
+  
 }
 
 module.exports = nextConfig
