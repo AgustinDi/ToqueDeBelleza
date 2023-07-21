03564fcfb9745656fddce2dd37d9cfba53080026
AgustinDi <agustindigiacinto18@gmail.com>
2023-07-21 13:20:58 -0300

    SliderFancy first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Categories from "./Components/Categories.jsx";
 import Slider from "./Components/Slider.jsx";
+import SliderFancy from "./Components/SliderFancy.jsx";
 import ProductsList from "./productos/ProductsList.jsx";
 
 export default function initialApp(){
@@ -7,6 +8,7 @@ export default function initialApp(){
     return (
         <>
             <Slider /> 
+            <SliderFancy />
             <h3>Nuestras Categorias:</h3>
             <Categories />
             <h3>Nuestros productos mas recomendados:</h3>
