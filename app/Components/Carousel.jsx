fdde43107cdddfd7df16873a9d8afb6eea55bba9
AgustinDi <agustindigiacinto18@gmail.com>
2023-07-24 23:56:41 -0300

    Carousel fancyapps working

---
+++ app/Components/Carousel.jsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { Carousel as CarouselFancy } from "@fancyapps/ui/dist/carousel/carousel.esm.js";
+import "@fancyapps/ui/dist/carousel/carousel.css";
+import './Carousel.css';
+
+export default function Carousel() {
+  const container = useRef(null)
+  const [images, setImages] = useState([
+      {
+          src: "/Banner 1 - Toque de Belleza.png",
+          index: 0
+      },
+      {
+          src: "/Banner 2 - Toque de Belleza.png",
+          index: 1
+      },
+      {
+          src: "/Loading.png",
+          index: 2
+      },
+      {
+          src: "/Loading.png",
+          index: 3
+      },
+  ])
+
+  useEffect(()=>{
+    const options = {  };
+    const result = new CarouselFancy(container.current, options)
+  },[])
+
+  return (
+     <div className="f-carousel" ref={container}>
+          {images.map(({src, index})=>
+              <div key={index} data-src={src} data-fancybox='Slider' data-caption={src} className="f-carousel__slide">
+                  <Image fill src={src} alt={src} quality={100} className='f-carousel__imageContainer' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
+              </div>
+          )}
+     </div>
+  )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,12 @@
 import Categories from "./Components/Categories.jsx";
-import Slider from "./Components/Slider.jsx";
-import SliderFancy from "./Components/SliderFancy.jsx";
+import Carousel from "./Components/Carousel.jsx";
 import ProductsList from "./productos/ProductsList.jsx";
 
 export default function initialApp(){
     
     return (
         <>
-            <Slider /> 
-            <SliderFancy />
+            <Carousel></Carousel>
             <h3>Nuestras Categorias:</h3>
             <Categories />
             <h3>Nuestros productos mas recomendados:</h3>
