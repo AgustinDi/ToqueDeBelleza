4a967fc7e5ff24e87cc6de3a313d4126d2b848b3
AgustinDi <agustindigiacinto18@gmail.com>
2023-08-04 10:38:41 -0300

    warning import blur - fixed

--- app/productos/ImageShow.jsx
+++ app/productos/ImageShow.jsx
@@ -1,12 +1,14 @@
 'use client'
 
 import Image from "next/image";
-import { blur as blurData } from '@/app/helper/blurData64.json';
+import blurData from '@/app/helper/blurData64.json';
 
 export default function ImageShow({ value }) {
+  const { blur } = blurData
+
   return (
      <div className='prcimageContainer'>
-          <Image src={value.images[0]} placeholder="blur" blurDataURL={blurData} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" fill alt={value.name}/>
+          <Image src={value.images[0]} placeholder="blur" blurDataURL={blur} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" fill alt={value.name}/>
      </div>
   )
 }
